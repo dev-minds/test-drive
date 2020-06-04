ae2169da15f4f38902f92254bfa86d4c93fec500
ansible <ansible@codename12>
2020-06-04 19:10:11 +0100

    Pushing spring boot on gradle

---
+++ app/dminds/src/main/java/com/devminds/dminds/DmindsApplication.java
@@ -0,0 +1,13 @@
+package com.devminds.dminds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmindsApplication.class, args);
+	}
+
+}
---
+++ app/dminds/src/test/java/com/devminds/dminds/DmindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devminds.dminds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
